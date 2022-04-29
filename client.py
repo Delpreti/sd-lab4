016edfc4d9746279c0f979cd10b7273db315a695
Delpreti <rddesiqueira@gmail.com>
2022-04-29 11:17:57 -0300

    execucao com threading

--- client.py
+++ client.py
@@ -4,25 +4,25 @@ import socket
 import sys
 
 HOST = 'localhost' # maquina onde esta o par passivo
-PORTA = 5001       # porta que o par passivo esta escutando
+PORTA = 5000       # porta que o par passivo esta escutando
 
 def main():
 
-    # verifica se existe um argumento passado na entrada
-    if len(sys.argv) != 2:
-        print("O nome do arquivo deve ser passado como argumento")
-        return
-
-    result = ""
     # estabelece a conexao com o servidor
     with socket.create_connection((HOST, PORTA)) as s:
-        s.sendall(bytes(sys.argv[1], 'utf-8')) # envia o nome do arquivo
-        result = str(s.recv(1024),  encoding='utf-8') # recebe as 5 palavras em uma string de resposta
 
-    if len(result) > 1: # verifica se a string voltou vazia (se algum erro ocorreu)
-        # imprime o resultado
-        print(f"As 5 palavras mais encontradas no arquivo {sys.argv[1]} foram: {result}")
-    else:
-        print("Erro, não foi possível obter os resultados.")
+        while True: 
+            msg = input("Digite uma mensagem ('fim' para terminar):")
+            if msg == 'fim':
+                break
+
+            s.sendall(bytes(msg, 'utf-8')) # envia o nome do arquivo
+            result = str(s.recv(1024),  encoding='utf-8') # recebe as 5 palavras em uma string de resposta
+
+            if len(result) > 1: # verifica se a string voltou vazia (se algum erro ocorreu)
+                # imprime o resultado
+                print(f"As 5 palavras mais encontradas no arquivo {msg} foram: {result}")
+            else:
+                print("Erro, não foi possível obter os resultados.")
 
 main()
--- server.py
+++ server.py
@@ -1,9 +1,46 @@
 # Lado passivo/server
 
 import socket, re
+import select
+import sys
+import threading
 
 HOST = ''     # '' possibilita acessar qualquer endereco alcancavel da maquina local
-PORTA = 5001  # porta onde chegarao as mensagens para essa aplicacao
+PORTA = 5000  # porta onde chegarao as mensagens para essa aplicacao
+
+#define a lista de I/O de interesse (jah inclui a entrada padrao)
+entradas = [sys.stdin]
+#armazena as conexoes ativas
+conexoes = {}
+#lock para acesso do dicionario 'conexoes'
+lock = threading.Lock()
+
+def atendeCliente(conn, addr):
+    lock.acquire()
+    conexoes[conn] = addr
+    lock.release()
+    print("Accepted connection from: ", addr)
+    while True:
+        # Recebe os dados enviados pelo cliente
+        print("Awating orders from: ", addr)
+        data = conn.recv(1024)
+        print("Teste")
+        if not data:
+            print(str(addr) + '-> encerrou')
+            lock.acquire()
+            del conexoes[conn] #retira o cliente da lista de conexoes ativas
+            lock.release()
+            conn.close() # encerra a conexao com o cliente
+            return
+
+        try:
+            # tenta realizar a leitura do arquivo
+            contents = read_file(str(data, encoding='utf-8'));
+            # se der certo, conta as palavras e envia o resultado pro cliente
+            conn.sendall(bytes(", ".join(high_words(word_count(contents), 5)), 'utf-8'))
+        except FileNotFoundError:
+            # se der errado, envia para o cliente uma string vazia
+            conn.sendall(bytes("Arquivo não existe", 'utf-8'))
 
 def word_count(string):
     """ Metodo para contar as ocorrencias de cada palavra em um determinado texto """
@@ -13,11 +50,11 @@ def word_count(string):
         result[word] = len(re.findall(r" ?" + word + r"\w", string))
     return result
 
-def high_words(counted, i=1):
+def high_words(counted, amount=1):
     # ordena o dicionario por valor
     # pega os i primeiros em uma lista de tuplas
     # e retorna apenas uma lista com as chaves
-    return dict(sorted(counted.items(), key=lambda item: item[1])[:i]).keys()
+    return dict(sorted(counted.items(), key=lambda item: item[1])[:amount]).keys()
 
 def read_file(filename):
     # realiza a leitura do arquivo
@@ -28,31 +65,29 @@ def main():
 
     # inicializa o servidor
     with socket.socket() as s:
+        # configura o socket
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORTA))
-        s.listen(1)
+        s.listen(5)
+        s.setblocking(False)
+        entradas.append(s)
+
         print(f"Server started, listening on port {PORTA}")
 
         while True:
-            # Aguarda uma nova conexao
-            conn, addr = s.accept()
-            with conn:
-                # Informa quem conectou
-                print("Accepted connection from", addr)
-
-                # Recebe os dados enviados pelo cliente
-                data = conn.recv(1024)
-                print(data)
-                if not data:
-                    break
-
-                try:
-                    # tenta realizar a leitura do arquivo
-                    contents = read_file(str(data, encoding='utf-8'));
-                    # se der certo, conta as palavras e envia o resultado pro cliente
-                    conn.sendall(bytes(", ".join(high_words(word_count(contents), 5)), 'utf-8'))
-                except FileNotFoundError:
-                    # se der errado, envia para o cliente uma string vazia
-                    conn.sendall(bytes("Arquivo não existe", 'utf-8'))
+            leitura, escrita, excecao = select.select(entradas, [], [])
+            for pronto in leitura:
+                if pronto == s:
+                    cliente = threading.Thread(target=atendeCliente, args=s.accept())
+                    cliente.start()
+                elif pronto == sys.stdin: #entrada padrao
+                    cmd = input()
+                    if cmd == 'fim': #solicitacao de finalizacao do servidor
+                        if not conexoes: #somente termina quando nao houver clientes ativos
+                            s.close()
+                            sys.exit()
+                        else: print("ha conexoes ativas")
+                    elif cmd == 'hist': #outro exemplo de comando para o servidor
+                        print(str(conexoes.values()))
 
 main()

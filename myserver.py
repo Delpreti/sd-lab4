9428295ab138e18853461a6e161e4f6b5b826d75
Delpreti <rddesiqueira@gmail.com>
2022-05-04 20:49:59 -0300

    application can now be updated without a server restart

--- myserver.py
+++ myserver.py
@@ -2,9 +2,10 @@ import socket
 import select
 import sys
 import threading
+from importlib import util
 
 class MyServer:
-    """ Classe para encapsular as funcionalidades de servidor """
+    """ Class to encapsulate the server functions """
     def __init__(self, hostname='', port=5000):
         self.hostname = hostname
         self.port = port
@@ -15,10 +16,13 @@ class MyServer:
         self.connections_lock = threading.Lock()
 
         self.method = None
+        self.app_name = None
+        self.app_path = None
 
         self.quit_flag = False
 
     def __enter__(self):
+        """ When the server is initialized, the internal socket must be configured """
         self._internal_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._internal_socket.bind((self.hostname, self.port))
         self._internal_socket.listen(5)
@@ -28,9 +32,14 @@ class MyServer:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """ When the server is finishing, the internal socket must be closed """
         self._internal_socket.close()
 
-    def run(self):
+    def run(self, app_name="app", app_path=""):
+        """ method to start the server """
+        self.set_application(app_name, app_path)
+        print(self.app_path)
+        self.update(False)
         while not self.quit_flag:
             reciever_list, _, _ = select.select(self.entradas, [], [])
             for reciever in reciever_list:
@@ -43,6 +52,23 @@ class MyServer:
         """ receives a function that takes a string as an argument (request), and returns a string (response) """
         self.method = function
 
+    def set_application(self, app_name, app_path):
+        """ receives strings containing the name and path to the file where the app() method is defined """
+        self.app_name = app_name
+        self.app_path = app_path
+
+    def update(self, verbose=True):
+        """
+        in case the app() method has been redefined, the server can update itself to run the new app
+        without having to close the socket that it currently reserves
+        """
+        if (self.app_name is None) or (self.app_path is None):
+            print("The application must be set first.")
+            return
+        self.set_external_method(MyServer.load_module(self.app_name, f"{self.app_path}{self.app_name}.py", piece="process"))
+        if verbose:
+            print("The server has been successfully updated.")
+
     def _spawn_client_thread(self):
         cliente = threading.Thread(target=self._method_caller, args=self._internal_socket.accept())
         cliente.start()
@@ -54,8 +80,15 @@ class MyServer:
                 print("Failed to exit the server: there are open connections")
             else: 
                 self.quit_flag = True
+        elif cmd == "forceexit":
+            for connection in self.connections:
+                connection.close()
+                self._unregister_connection(connection)
+            self.quit_flag = True
         elif cmd == "hist":
             print(str(self.connections.values()))
+        elif cmd == "update":
+            self.update()
 
     def _register_connection(self, conn, addr):
         self.connections_lock.acquire()
@@ -79,3 +112,14 @@ class MyServer:
                 return
             conn.sendall(bytes(self.method(data), 'utf-8'))
 
+    @staticmethod
+    def load_module(module_name, module_path, piece=None):
+        """ Method used for dinamic module importing """
+        # print(f"Loading module {module_name}")
+        spec = util.spec_from_file_location(module_name, module_path)
+        module = util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+
+        if piece:
+            return getattr(module, piece)
--- server.py
+++ server.py
@@ -1,55 +1,11 @@
 # Lado passivo/server
 
-import re
 from myserver import MyServer
-
-def high_five(text_string):
-    """ Metodo que retorna as 5 palavras que mais ocorrem dentro de um determinado texto. """
-    try:
-        contents = read_file(str(text_string, encoding='utf-8'));
-        return ", ".join(high_words(word_count(contents), 5))
-    except FileNotFoundError:
-        return "Arquivo não encontrado."
-
-def word_count(string):
-    """ Metodo para contar as ocorrencias de cada palavra em um determinado texto """
-    result = {}
-    unique_words = set(re.split(r"\W+", string))
-    for word in unique_words:
-        result[word] = len(re.findall(r"^" + word + r"$|\W" + word + r"$|^" + word + r"\W|\W" + word + r"\W", string))
-    del result[""]
-    return result
-
-def high_words(counted, amount=1):
-    # ordena o dicionario por valor
-    # pega os i primeiros em uma lista de tuplas
-    # e retorna apenas uma lista com as chaves
-    return dict(sorted(counted.items(), key=lambda item: item[1])[:amount]).keys()
-
-def read_file(filename):
-    with open(filename, "r") as f:
-        return f.read()
 
 def main():
     # passar hostname e porta aqui para conectar a alguem diferente
     with MyServer() as server:
-        # configura a funcao a ser executada pelo servidor para processar as requisicoes
-        server.set_external_method(high_five)
         # inicia o servidor
         server.run()
 
 main()
-
-"""
-usado apenas para debug
-
-def all_counts(text_string):
-    try:
-        contents = read_file(str(text_string, encoding='utf-8'));
-        result = ""
-        for key, value in word_count(contents).items():
-            result += f"[{key}:{value}]"
-        return result
-    except FileNotFoundError:
-        return "Arquivo não encontrado."
-"""
